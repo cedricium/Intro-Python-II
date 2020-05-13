6bfe61ca320cb87c0b226fc67bf2128ae2ea516c
Cedricium <amaya.cedric@gmail.com>
2020-05-12 20:23:46 -0700

    Complete Day 1 exercises

--- src/adv.py
+++ src/adv.py
@@ -1,7 +1,7 @@
+from player import Player
 from room import Room
 
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -21,9 +21,7 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -33,19 +31,60 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
-# Main
-#
+ACTIONS = {
+    'n': 'n_to',
+    'e': 'e_to',
+    's': 's_to',
+    'w': 'w_to',
+}
 
-# Make a new player object that is currently in the 'outside' room.
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+def parse_user_action():
+    action = input('What is your next move: ')
+    return action.lower()
+
+
+def handle_action(player, action):
+    if action == 'q':
+        return False
+    elif action in ('n', 'e', 's', 'w'):
+        direction = ACTIONS[action]
+        room = getattr(player.current_room, direction)
+        if room != None:
+            player.current_room = room
+        else:
+            print('Unable to move %s, try again.' % action)
+    return True
+
+
+def main():
+    print('An adventure awaits…')
+    # Make a new player object that is currently in the 'outside' room.
+
+    # Write a loop that:
+    #
+    # * Prints the current room name
+    # * Prints the current description (the textwrap module might be useful here).
+    # * Waits for user input and decides what to do.
+    #
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+    #
+    # If the user enters "q", quit the game.
+    player = Player('Augustine', room['outside'])
+    continue_playing = True
+
+    while continue_playing:
+        # print current room and description
+        # get user input (`parse_user_action()`)
+        print(player.current_room)
+
+        action = parse_user_action()
+        continue_playing = handle_action(player, action)
+        #   - if n,s,w,e: attempt to move in given direction (error if move not possible)
+        #   - elif q: quit the game (`continue_playing = false`)
+        #   - else: ignore and redo
+
+
+if __name__ == "__main__":
+    main()
